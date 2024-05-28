fdde9e6b80f42e5466048db7f8d197678eb405dd
ivanpopovicib <ivan.popovic@myib.com>
2024-05-28 15:25:36 +0200

    add: custom theme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PreviewComponent } from './preview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PreviewComponent,
+  },
+];
